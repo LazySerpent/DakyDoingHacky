ae8bc48fb83a51b18fcaab7045490841e5344774
ivFiz <spy.boy.ksa@gmail.com>
2021-03-27 04:29:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -115,7 +115,65 @@ client.on('message', message => {
       )
     message.channel.send(embed);
   }
+    if (message.content === 'sl') {
+      const embed = new MessageEmbed()
+        .setTitle('قائمة  الأغاني')
+        .setColor(0x11AABB)
+        .addFields(
+          { name: 'اكتب  رقم  الأغنية  وراح  تشتغل  معك', value: 'تأكد  من  كتابة  s  قبل  أي  رقم' },
+          { name: '\u200B', value: '\u200B' },
+          { name: '\u200B   ليلة تمرين   \u200b', value: '1', inline: true },
+          { name: '\u200B   عطني المحبه   \u200B', value: '2', inline: true },
+          { name: '\u200B   اليوم يمكن تقولي   \u200B', value: '3', inline: true },
+          { name: '\u200B   انتهينا   \u200B', value: '4', inline: true },
+          { name: '\u200B   مرحبا بك يا هلا   \u200B', value: '5', inline: true },
+          { name: '\u200B   ماتقول لنا صاحب   \u200B', value: '6', inline: true },
+          { name: '\u200B   لا وعد   \u200B', value: '7', inline: true },
+          { name: '\u200B   تعلق قلبي   \u200B', value: '8', inline: true },
+          { name: '\u200B   من بعد مزح ولعب   \u200B', value: '9', inline: true },
+          { name: '\u200B   احرجتني   \u200B', value: '10', inline: true },
+          { name: '\u200B   أنا راجع أشوفك    \u200B', value: '11', inline: true },
+          { name: '\u200B   اغنم زمانك    \u200B', value: '12', inline: true },
+          { name: '\u200B   احبك لو تكون حاضر    \u200B', value: '13', inline: true },
+          { name: '\u200B   مقادير    \u200B', value: '14', inline: true },
+          { name: '\u200B   العطر    \u200B', value: '15', inline: true },
+          { name: '\u200B   وردك يا زارع الورد    \u200B', value: '16', inline: true },
+          { name: '\u200B   زمان الصمت    \u200B', value: '17', inline: true },
+          { name: '\u200B   اعترف لك    \u200B', value: '18', inline: true },
+          { name: '\u200B   صعب السؤال    \u200B', value: '19', inline: true },
+          { name: '\u200B   انا العاشق    \u200B', value: '20', inline: true },
+          { name: '\u200B   قصت ظفايرها    \u200B', value: '21', inline: true },
+          { name: '\u200B   جتني تقول    \u200B', value: '22', inline: true },
+          { name: '\u200B   مهتم    \u200B', value: '23', inline: true },
+        )
+      message.channel.send(embed);
+    }
   if (message.content === 'slist2') {
+    const embed = new MessageEmbed()
+      .setTitle('قائمة  الأغاني')
+      .setColor(0x11AABB)
+      .addFields(
+        { name: 'اكتب  رقم  الأغنية  وراح  تشتغل  معك', value: 'تأكد  من  كتابة  s  قبل  أي  رقم' },
+        { name: '\u200B', value: '\u200B' },
+        { name: '\u200B   كلام البارح اتغير طلال     \u200B', value: '24', inline: true },
+        { name: '\u200B   فرح العمر     \u200B', value: '25', inline: true },
+        { name: '\u200B   عندك امل    \u200B', value: '26', inline: true },
+        { name: '\u200B   الله يرد خطاك   \u200B', value: '27', inline: true },
+        { name: '\u200B   قولوا للغالي    \u200B', value: '28', inline: true },
+        { name: '\u200B   ظالم ولكن    \u200B', value: '29', inline: true },
+        { name: '\u200B   طاول الصبر    \u200B', value: '30', inline: true },
+        { name: '\u200B   أغراب    \u200B', value: '31', inline: true },
+        { name: '\u200B   نام الطريق    \u200B', value: '32', inline: true },
+        { name: '\u200B   أبتدت تحلى الحياة    \u200B', value: '33', inline: true },
+        { name: '\u200B   مين فتن بيني وبينك    \u200B', value: '34', inline: true },
+        { name: '\u200B   سويعات الاصيل    \u200B', value: '35', inline: true },
+        { name: '\u200B   مر بي    \u200B', value: '36', inline: true },
+        { name: '\u200B   أجاذبك الهوى    \u200B', value: '37', inline: true },
+        { name: '\u200B   مسرحية    \u200B', value: '38', inline: true },
+      )
+    message.channel.send(embed);
+  }
+  if (message.content === 'sl2') {
     const embed = new MessageEmbed()
       .setTitle('قائمة  الأغاني')
       .setColor(0x11AABB)
@@ -157,7 +215,42 @@ client.on('message', message => {
       )
     message.channel.send(embed);
   }
+  if (message.content === 'al') {
+    const embed = new MessageEmbed()
+      .setTitle('قائمة  أغاني عبادي الجوهر')
+      .setColor(0x11AABB)
+      .addFields(
+        { name: 'اكتب  رقم  الأغنية  وراح  تشتغل  معك', value: 'تأكد  من  كتابة  a  قبل  أي  رقم' },
+        { name: '\u200B', value: '\u200B' },
+        { name: '\u200B    من بعد مزح ولعب    \u200b', value: '1', inline: true },
+        { name: '\u200B   عيونك آخر آمالي   \u200B', value: '2', inline: true },
+        { name: '\u200B   تدرين وأدري بنفترق   \u200B', value: '3', inline: true },
+        { name: '\u200B   قالوا ترى   \u200B', value: '4', inline: true },
+        { name: '\u200B   يا شوق انا قلبي   \u200B', value: '5', inline: true },
+        { name: '\u200B   كلمة ولو جبر خاطر   \u200B', value: '6', inline: true },
+        { name: '\u200B   أحبك لو تكون حاضر   \u200B', value: '7', inline: true },
+      )
+    message.channel.send(embed);
+  }
   if (message.content === 'klist') {
+    const embed = new MessageEmbed()
+      .setTitle('قائمة  أغاني خالد عبدالرحمن')
+      .setColor(0x11AABB)
+      .addFields(
+        { name: 'اكتب  رقم  الأغنية  وراح  تشتغل  معك', value: 'تأكد  من  كتابة  k  قبل  أي  رقم' },
+        { name: '\u200B', value: '\u200B' },
+        { name: '\u200B    خذني بقايا جروح    \u200b', value: '1', inline: true },
+        { name: '\u200B   وشلون مغليك   \u200B', value: '2', inline: true },
+        { name: '\u200B   الذاهبه   \u200B', value: '3', inline: true },
+        { name: '\u200B   يا صاحبي   \u200B', value: '4', inline: true },
+        { name: '\u200B   تقوى الهجر   \u200B', value: '5', inline: true },
+        { name: '\u200B   انتي اجمل   \u200B', value: '6', inline: true },
+        { name: '\u200B   العطا   \u200B', value: '7', inline: true },
+        { name: '\u200B   يا ظالمة   \u200B', value: '8', inline: true },
+      )
+    message.channel.send(embed);
+  }
+  if (message.content === 'kl') {
     const embed = new MessageEmbed()
       .setTitle('قائمة  أغاني خالد عبدالرحمن')
       .setColor(0x11AABB)
@@ -187,7 +280,37 @@ client.on('message', message => {
       )
     message.channel.send(embed);
   }
+  if (message.content === 'xl') {
+    const embed = new MessageEmbed()
+      .setTitle('قائمة جانبية')
+      .setColor(0x11AABB)
+      .addFields(
+        { name: '\u200B', value: '\u200B' },
+        { name: '\u200B    من حب من قلب ماهون    \u200b', value: '1', inline: true },
+        { name: '\u200B   خلصت من جملة الخلان   \u200B', value: '2', inline: true },
+        { name: '\u200B   حد مثلي   \u200B', value: '3', inline: true },
+      )
+    message.channel.send(embed);
+  }
   if (message.content === 'shelp') {
+    const embed = new MessageEmbed()
+      .setTitle('قائمة  الأوامر')
+      .setColor(0x11AABB)
+      .addFields(
+        { name: 'اكتب الأمر وراح يشتغل معك', value: 'تأكد من كتابة s قبل أي أمر' },
+        { name: '\u200B', value: '\u200B' },
+        { name: '\u200B   shelp   \u200b', value: 'يعطيك القائمة ذي', inline: true },
+        { name: '\u200B   slist   \u200B', value: 'لستت أغاني المرحوم طلال مداح (يكتب بعدها:1 أو 2إلخ...)', inline: true },
+        { name: '\u200B   alist   \u200B', value: 'لستت أغاني عبادي الجوهر (يكتب بعدها:1 أو 2إلخ...)', inline: true },
+        { name: '\u200B   klist   \u200B', value: 'لستت أغاني خالد عبدالرحمن (يكتب بعدها:1 أو 2إلخ...)', inline: true },
+        { name: '\u200B   s او k او a (يتبعها رقم الأغنية)   \u200B', value: 'لتشغيل الأغنية المختارة من اللسته', inline: true },
+        { name: '\u200B   splay   \u200B', value: 'تشغيل اغنية من اليوتيوب عن طريق رابط', inline: true },
+        { name: '\u200B   sskip   \u200B', value: 'تسكيب اغنية ', inline: true },
+        { name: '\u200B   sdis   \u200B', value: 'إيقاف البوت', inline: true },
+      )
+    message.channel.send(embed);
+  }
+  if (message.content === 'sh') {
     const embed = new MessageEmbed()
       .setTitle('قائمة  الأوامر')
       .setColor(0x11AABB)
@@ -581,10 +704,11 @@ function stop(message, serverQueue) {
     );
     
   if (!serverQueue)
+  return message.channel.send("مافيه شيء شغال");
     
   serverQueue.songs = [];
   serverQueue.connection.dispatcher.end();
-  return message.channel.send("تم");
+  return message.channel.send("تم")
 }
 
 function play(guild, song) {
