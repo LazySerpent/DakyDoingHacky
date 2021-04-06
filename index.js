8505a68d941834e12964febfe8b8f661a56f5675
ivFiz <spy.boy.ksa@gmail.com>
2021-04-07 01:07:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -862,19 +862,4 @@ client.on('voiceStateUpdate', (oldState, newState) => {
   
 });
 
-client.on('message', message => {
-  let xprefix = '!'
-  let args = message.content
-    .slice(xprefix.length)
-    .trim()
-    .split(' ') //arguments
-  let command = args.shift().toLowerCase() //shifts args to lower case letters
-
-  if (command === 'say') {
-    let say = args.join(' ') //space
-    message.delete() //deletes the message you sent
-    client.channels.cache.get("730869961475489903").send(say)
-  }
-})
-
 client.login(process.env.token);
